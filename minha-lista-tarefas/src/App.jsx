eff48f3e55ac8d8f83917b045dd1c3803bff7c0c
MARIA BEATRIZ NASCIMENTO ALVES <maria.alves447@etec.sp.gov.br>
2026-03-19 14:26:40 -0300

    adiciona funcionalidade de lista de tarefas

---
+++ minha-lista-tarefas/src/App.jsx
@@ -0,0 +1,13 @@
+import ListaTarefas from './components/ListaTarefas';
+import './App.css';
+
+function App() {
+  return (
+    <>
+      <h1>Gerenciador de Tarefas</h1>
+      <ListaTarefas />
+    </>
+  );
+}
+
+export default App;
